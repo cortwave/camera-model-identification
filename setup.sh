1f610c1a697076be3bc769b276109e9ff2187bfc
Dmitry Pranchuk <d.pranchuk@gmail.com>
2018-01-07 21:27:53 +0300

    setup script

--- setup.sh
+++ setup.sh
@@ -1,3 +1,19 @@
 #!/usr/bin/env bash
 
 git clone https://github.com/Cadene/pretrained-models.pytorch.git
+
+mkdir data
+mkdir results
+
+cd data
+
+kg download -u '$1' -p '$2' -c 'sp-society-camera-model-identification'
+
+unzip test.zip
+unzip train.zip
+
+cd ././src
+
+python3 scripts/split_folds.py
+
+echo 'Done!'
