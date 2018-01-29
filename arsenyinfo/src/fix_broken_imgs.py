2f896425a545626524a9cbc4d9831414f48a9879
Arseny Kravchenko <me@arseny.info>
2018-01-30 01:15:10 +0300

    fix suspicious images

---
+++ arsenyinfo/src/fix_broken_imgs.py
@@ -0,0 +1,32 @@
+from glob import glob
+from os import remove
+
+from fire import Fire
+from tqdm import tqdm
+import cv2
+from glog import logger
+
+
+def fix_and_check(x):
+    with open(x, 'rb') as f:
+        data = f.read()
+        check_chars = data[-2:]
+        if check_chars == b'\xff\xd9':
+            return
+    with open(x, 'wb') as f:
+        f.write(data)
+
+    img = cv2.imread(x)
+    if img is None or img[-10:, :, :].std() == 0:
+        logger.info(f'{x} is removed')
+        remove(x)
+
+
+def main(path):
+    files = glob(path)
+    for f in tqdm(files):
+        fix_and_check(f)
+
+
+if __name__ == '__main__':
+    Fire(main)
