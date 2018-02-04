fc52d1c9f4e85bb9ec31942764387afc222018f9
Arseny Kravchenko <me@arseny.info>
2018-02-04 19:28:49 +0300

    minor fixes

--- arsenyinfo/src/fix_broken_imgs.py
+++ arsenyinfo/src/fix_broken_imgs.py
@@ -6,6 +6,8 @@ from tqdm import tqdm
 import cv2
 from glog import logger
 
+from src.utils import get_img_attributes
+
 
 def fix_and_check(x):
     with open(x, 'rb') as f:
@@ -20,6 +22,18 @@ def fix_and_check(x):
     if img is None or img[-5:, :, :].std() == 0:
         logger.info(f'{x} is removed')
         remove(x)
+        return
+
+    q, soft = get_img_attributes(x)
+    if q < 90:
+        logger.info(f'{x} is removed: quality is {q}')
+        remove(x)
+        return
+
+    if any(map(lambda x: x in soft, ('editor', 'adobe', 'aperture'))):
+        logger.info(f'{x} is removed: software is {soft}')
+        remove(x)
+        return
 
 
 def main(path):
--- arsenyinfo/src/utils.py
+++ arsenyinfo/src/utils.py
@@ -1,6 +1,34 @@
 import logging
+import subprocess
 
 logging.basicConfig(level=logging.INFO,
                     format='%(levelname)s: %(name)s: %(message)s (%(asctime)s; %(filename)s:%(lineno)d)',
                     datefmt="%Y-%m-%d %H:%M:%S", )
 logger = logging.getLogger(__name__)
+
+
+def get_img_attributes(fname):
+    s = subprocess.run([f'identify', '-verbose', f'{fname}'],
+                       stdout=subprocess.PIPE,
+                       stderr=subprocess.PIPE,
+                       )
+
+    s = s.stdout.decode().split('\n')
+
+    try:
+        quality = int(list(filter(lambda x: 'Quality' in x, s))[0].split(': ')[-1])
+    except Exception:
+        logger.exception(f'Can not parse {fname} quality')
+        quality = 0
+
+    try:
+        soft = [x for x in s if 'Software' in x]
+        if soft:
+            soft = soft[0].split(': ')[-1]
+        else:
+            soft = ''
+    except Exception:
+        logger.exception(f'Can not parse {fname} software')
+        soft = ''
+
+    return quality, soft
